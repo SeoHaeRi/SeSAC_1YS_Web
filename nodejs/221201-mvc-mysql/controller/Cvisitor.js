77e695aac656b0aa9865fc7689efb9eb6344b8b1
SeoHaeRi <jackie100425@naver.com>
2022-12-03 18:37:45 +0900

    feat: update, delete 기능 추가

--- nodejs/221201-mvc-mysql/controller/Cvisitor.js
+++ nodejs/221201-mvc-mysql/controller/Cvisitor.js
@@ -14,3 +14,29 @@ exports.register = (req, res) => {
         res.send(String(id));
     })
 }
+
+exports.delete = (req, res) => {
+    // mysql req.body.id에 해당하는 데이터를 delete -> model
+    // 서버 응답 res.send
+
+    Visitor.delete_visitor(req.body.id, function () {
+        res.send(true);
+    })
+}
+
+exports.get_visitor_by_id = (req, res) => {
+    // req.query.id 에 해당하는 데이터를 조회
+    // 서버 응답 > 조회한 데이터
+    Visitor.get_visitor_by_id_model(req.query.id, function (rows) {
+        console.log(rows);
+        res.send(rows);
+    })
+}
+
+exports.update_visitor = (req, res) => {
+    // req.body 데이터를 mysql에 update 할 수 있도록
+    // 서버 응답
+    Visitor.update_visitor(req.body, function () {
+        res.send(true);
+    })
+}
--- nodejs/221201-mvc-mysql/model/Visitor.js
+++ nodejs/221201-mvc-mysql/model/Visitor.js
@@ -11,6 +11,7 @@ const cnn = mysql.createConnection({
 // 콜백 함수
 exports.get_visitor = (cb) => {
     var sql = 'SELECT * FROM visitor';
+    // cnn.query() 함수가 실행이 오래 걸림, 결과가 나올 때 까지 기다려주지 않아서 콜백 함수 사용
     cnn.query(sql, (err, rows) => {
         if (err) throw err;
         console.log("visitors : ", rows);
@@ -19,12 +20,44 @@ exports.get_visitor = (cb) => {
     })
 }
 
+// 등록
 exports.register_visitor = (info, cb) => {
+    // info = req.body
     var sql = `INSERT into visitor (name, comment) values ('${info.name}', '${info.comment}');`;
     cnn.query(sql, (err, result) => {
         if (err) throw err;
         console.log("insert result : ", result);
 
         cb(result.insertId);
+    })
+}
+
+// 삭제
+exports.delete_visitor = (id, cb) => {
+    var sql = `DELETE from visitor where id = ${id};`
+
+    cnn.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log("delete result : ", result);
+        cb();
+    })
+}
+
+// 수정
+exports.get_visitor_by_id_model = (id, cb) => {
+    var sql = `SELECT * from visitor where id = ${id};`
+    cnn.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.log("select by id : ", rows);
+        cb(rows[0]);
+    })
+}
+
+exports.update_visitor = (info, cb) => {
+    var sql = `UPDATE visitor set name='${info.name}', comment='${info.comment}' where id=${info.id};`;
+    cnn.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log("update : ", result);
+        cb();
     })
 }
